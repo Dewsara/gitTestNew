c0704181eede177dd9900ef8dd344c19e687f0d6
Dewsara <dewsara6@gmail.com>
2021-04-18 00:42:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+//Importing Express
+const express = require('express');
+//Importing Mongoose
+const mongo =  require('mongoose');
+//Importing bodyParser
+const bParser = require('body-parser');
+//Importing Cors
+const cors = require('cors');
+
+const app = express();
+
+require('dotenv').config();
+
+app.use(bParser.urlencoded({extended: true}));
+app.use(bParser.json());
+app.use(cors());
+
+app.use("/SensorAPI", require("./Route/FireSensorRoute"));
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(422).send({ error: err.message });
+});
+
+const url = 'mongodb+srv://DSAssignment:DSAssignment@cluster0-vz7sv.mongodb.net/test?retryWrites=true&w=majority' ;
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+};
+
+//Creating the connection with MongoDB.
+mongo.connect(
+    url, options
+).then(() => {
+    app.listen(5000);
+}).catch((err) => {
+    console.log(err);
+});
+
+
